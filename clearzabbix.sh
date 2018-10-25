5aceecb51265a909d548ef6faaa5607374656b7f
MDipper <MDipper@users.noreply.github.com>
2018-10-25 14:55:10 +0800

    Update clearzabbix.sh

--- clearzabbix.sh
+++ clearzabbix.sh
@@ -4,35 +4,33 @@ User="zabbix"
 
 Passwd="zabbix"
 
-HIS_Date=`date -d $(date -d "-30 day" +%Y%m%d) +%s` #取30天之前的时间戳
-
-TRE_Date=`date -d $(date -d "-30 day" +%Y%m%d) +%s` #取30天之前的时间戳
+Date=`date -d $(date -d "-30 day" +%Y%m%d) +%s` #取30天之前的时间戳
 
 $(which mysql) -u${User} -p${Passwd} -e "
 
 use zabbix;
 
-DELETE FROM history WHERE clock < $HIS_Date;
+DELETE FROM history WHERE clock < $Date;
 
 optimize table history;
 
-DELETE FROM history_str WHERE clock < $HIS_Date;
+DELETE FROM history_str WHERE clock < $Date;
 
 optimize table history_str;
 
-DELETE FROM history_uint WHERE clock < $HIS_Date;
+DELETE FROM history_uint WHERE clock < $Date;
 
 optimize table history_uint;
 
-DELETE FROM trends WHERE clock < $TRE_Date;
+DELETE FROM trends WHERE clock < $Date;
 
 optimize table trends;
 
-DELETE FROM trends_uint WHERE clock < $TRE_Date;
+DELETE FROM trends_uint WHERE clock < $Date;
 
 optimize table trends_uint;
 
-DELETE FROM events WHERE clock < $TRE_Date;
+DELETE FROM events WHERE clock < $Date;
 
 optimize table events;
 "
